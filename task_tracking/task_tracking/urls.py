d1296df1fa67589124ca344cad9e72f9da653d98
qsire2 <158807774+qsire2@users.noreply.github.com>
2026-03-07 19:01:18 +0200

    jj

--- task_tracking/task_tracking/urls.py
+++ task_tracking/task_tracking/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("tasks.urls")),
+    path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- task_tracking/tasks/views.py
+++ task_tracking/tasks/views.py
@@ -18,7 +18,7 @@ from .mixins import UserIsOwnerMixin
 class TaskListView(ListView):
     model = models.Task
     context_object_name = "tasks"
-    template_name = "tasks/task_list.html"
+    template_name = "tasks/templates/task_list.html"
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -32,14 +32,15 @@ class TaskListView(ListView):
         context["form"] = TaskFilterForm(self.request.GET)
         return context
 
+
 class TaskDetailView(LoginRequiredMixin, DetailView):
     model = models.Task
     context_object_name = "task"
-    template_name = 'tasks/task_detail.html'
+    template_name = "tasks/templates/task_detail.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['comment_form'] = CommentForm()  # Додаємо порожню форму коментаря в контекст
+        context["comment_form"] = CommentForm()
         return context
 
     def post(self, request, *args, **kwargs):
@@ -49,8 +50,79 @@ class TaskDetailView(LoginRequiredMixin, DetailView):
             comment.author = request.user
             comment.task = self.get_object()
             comment.save()
-            return redirect('tasks:task-detail', pk=comment.task.pk)
-        else:
-            # Тут можна обробити випадок з невалідною формою
-            pass
+            return redirect("tasks:task-detail", pk=comment.task.pk)
 
+
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = models.Task
+    form_class = TaskForm
+    template_name = "tasks/templates/task_form.html"
+    success_url = reverse_lazy("tasks:task-list")
+
+    def form_valid(self, form):
+        form.instance.creator = self.request.user
+        return super().form_valid(form)
+
+
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
+    model = models.Task
+    form_class = TaskForm
+    template_name = "tasks/templates/task_update_form.html"
+    success_url = reverse_lazy("tasks:task-list")
+
+
+class TaskDeleteView(LoginRequiredMixin, DeleteView):
+    model = models.Task
+    template_name = "tasks/templates/task_delete_confirmation.html"
+    success_url = reverse_lazy("tasks:task-list")
+
+
+class TaskCompleteView(LoginRequiredMixin, View):
+    def get(self, request, pk):
+        task = get_object_or_404(models.Task, pk=pk)
+        task.completed = True
+        task.save()
+        return redirect("tasks:task-detail", pk=pk)
+
+
+class CommentUpdateView(LoginRequiredMixin, UpdateView):
+    model = models.Comment
+    form_class = CommentForm
+    template_name = "tasks/templates/edit_comment.html"
+
+    def get_success_url(self):
+        return self.object.task.get_absolute_url()
+
+
+class CommentDeleteView(LoginRequiredMixin, DeleteView):
+    model = models.Comment
+    template_name = "tasks/templates/delete_comment.html"
+
+    def get_success_url(self):
+        return self.object.task.get_absolute_url()
+
+
+class CommentLikeToggle(LoginRequiredMixin, View):
+    def get(self, request, pk):
+        comment = get_object_or_404(models.Comment, pk=pk)
+
+        if request.user in comment.likes.all():
+            comment.likes.remove(request.user)
+        else:
+            comment.likes.add(request.user)
+
+        return redirect(request.META.get("HTTP_REFERER"))
+
+
+class CustomLoginView(LoginView):
+    template_name = "tasks/templates/login.html"
+
+
+class CustomLogoutView(LogoutView):
+    next_page = reverse_lazy("tasks:login")
+
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = "tasks/templates/register.html"
+    success_url = reverse_lazy("tasks:login")
